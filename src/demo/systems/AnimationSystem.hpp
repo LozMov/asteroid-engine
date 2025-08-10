d94297aa5809e2f90e4718d0349dcb994ddf267d
LozMov <i@lozmov.com>
2025-08-09 21:57:31 -0700

    Add AnimationSystem

---
+++ src/demo/systems/AnimationSystem.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "engine/ecs/Registry.hpp"
+#include "engine/ecs/System.hpp"
+
+#include "../components/Animation.hpp"
+#include "../components/Sprite.hpp"
+
+namespace astd::systems {
+
+class AnimationSystem : public ast::System<components::Animation, components::Sprite> {
+public:
+    AnimationSystem(ast::Registry& registry) : System(registry) {}
+
+    void update(float dt) override {
+        for (auto entity : entities_) {
+            auto* animation = registry_.get<components::Animation>(entity);
+            if (animation->playing) {
+                auto* sprite = registry_.get<components::Sprite>(entity);
+                animation->frameTimeElapsed += dt;
+                if (animation->frameTimeElapsed >= animation->frameDuration) {
+                    animation->frameTimeElapsed = 0.0f;
+                    if (++sprite->frameIndex == sprite->frameCount) {
+                        if (animation->loop) {
+                            sprite->frameIndex = 0;
+                        } else {
+                            // Stop at the last frame if not looping
+                            --sprite->frameIndex;
+                            animation->playing = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+}  // namespace astd::systems
